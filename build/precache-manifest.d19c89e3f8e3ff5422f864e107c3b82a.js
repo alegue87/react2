8c28b06ef262609db945786e63315ba0154bbe45
alessio <alegue87@gmail.com>
2019-12-27 18:35:07 +0100

    ini

---
+++ build/precache-manifest.d19c89e3f8e3ff5422f864e107c3b82a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1651790a1ceab9880ab841fc4e6029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c602b6728736f4e7f5f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c602b6728736f4e7f5f",
+    "url": "/static/js/main.c0b7b600.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
