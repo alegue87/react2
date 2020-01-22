7372bf42a0fe7f7f3611a4cbec8e20dbeb20f752
alessio <alegue87@gmail.com>
2020-01-22 15:22:34 +0100

    refix

--- src/config.js
+++ src/config.js
@@ -2,15 +2,10 @@
 var BASE_URL,
     HOST_PORT;
 
-if(process.env.NODE_ENV == 'production'){
-    
-    BASE_URL = 'https://expressapireact2.herokuapp.com:3000/testAPI'
-    HOST_PORT = 'https://expressapireact2.herokuapp.com:3000'
-}
-else{
-    BASE_URL = 'http://localhost:3000/testAPI'
-    HOST_PORT = 'http://localhost:3000'
-}
+
+BASE_URL = 'https://expressapireact2.herokuapp.comtestAPI'
+HOST_PORT = 'https://expressapireact2.herokuapp.com'
+
 
 
 module.exports = {BASE_URL, HOST_PORT};
