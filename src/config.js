aab24e2376eb410ec110f93cc3f20d7f0919e288
alessio <alegue87@gmail.com>
2020-01-22 15:10:13 +0100

    fix prod/dev

--- src/config.js
+++ src/config.js
@@ -1,17 +1,18 @@
 
 var BASE_URL,
     HOST_PORT;
-var host = window.location.href.split('/')[2].split(':')[0] 
-if(host == 'localhost'){
-    BASE_URL = 'http://localhost:3000/testAPI'
-    HOST_PORT = 'http://localhost:3000'    
-}
-else{
+
+if(process.env.NODE_ENV == 'production'){
+    
     BASE_URL = 'https://expressapireact2.herokuapp.com:3000/testAPI'
     HOST_PORT = 'https://expressapireact2.herokuapp.com:3000'
+}
+else{
+    BASE_URL = 'http://localhost:3000/testAPI'
+    HOST_PORT = 'http://localhost:3000'
 }
 
 
 module.exports = {BASE_URL, HOST_PORT};
 
-console.log("host:"+host)
+console.log(process.env.NODE_ENV)
