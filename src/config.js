7a32c1a59c6f7967ae39299c63529c350d744910
alegue87 <alegue87@gmail.com>
2020-01-25 20:38:11 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+var BASE_URL,
+    HOST_PORT;
+
+
+BASE_URL = 'https://expressapireact2.herokuapp.com/testAPI'
+HOST_PORT = 'https://expressapireact2.herokuapp.com'
+
+module.exports = {BASE_URL, HOST_PORT};
